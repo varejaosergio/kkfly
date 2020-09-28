0d08af0c25a03b3aecb0ecb20964a1d994913955
Sérgio Schwartz Varejão <43224989+varejaosergio@users.noreply.github.com>
2020-09-28 16:17:39 -0700

    kkfly heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL = window.location.hostname.includes('localhost')
 ? 'http://localhost:8080'
-: 'https://krystian-kymerson.herokuapp.com/';
+: 'https://kkfly.herokuapp.com/';
 
 export default {
     URL,
